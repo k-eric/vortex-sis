c3bb73e090386fcbe64208b2b04c6844b1c177dd
Keric <ekwendo5@gmail.com>
2026-08-19 02:39:37 +0300

    Fix appwrite config

--- src/lib/appwrite.js
+++ src/lib/appwrite.js
@@ -1,10 +1,11 @@
-import { Client, Account, Databases, ID } from 'appwrite';
+import { Client, Account, Databases } from 'appwrite';
 
-const client = new Client()
+const client = new Client();
+
+client
     .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
     .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
-const account = new Account(client);
-const databases = new Databases(client);
-
-export { client, account, databases, ID };
+export const account = new Account(client);
+export const databases = new Databases(client);
+{ID} from 'appwrite';
